f0dd2772f14faaf78df5434b3a09199b3e7329b4
Hurry-sh <141551702+Hurry-sh@users.noreply.github.com>
2024-07-18 09:34:24 +0530

    added files

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,8 +5,22 @@ const Footer = () => {
     return (
         <footer className="footer">
             <div className="footer__container container">
-                <h2 className="footer__title">Made with ❤️ by</h2> 
-                <h2 className="footer__name"> Harish K. </h2>
+                <h2 className="footer__title">Made with ❤️ by<h2 className="footer__name"> Harish K </h2>.</h2> 
+                
+
+                {/* <ul className="footer__list">
+                    <li>
+                        <a href="#about" className="footer__link">About</a>
+                    </li>
+
+                    <li>
+                        <a href="#portfolio" className="footer__link">Projects</a>
+                    </li> */}
+
+                    {/* <li>
+                        <a href="#about" className="footer__link">About</a>
+                    </li> */}
+                {/* </ul> */}
             </div>
         </footer>
     )
