3f474b76939ac6e37668a9915d216676f671510f
Hurry-sh <141551702+Hurry-sh@users.noreply.github.com>
2024-08-06 09:05:14 +0530

    final commit

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -14,7 +14,7 @@ const Info = () => {
             <i className='bx bx-award about__icon'></i>
 
                 <h3 className="about__title">Scholarships</h3>
-                <span className="about__subtitle">3x Distinction Scholar.</span>
+                <span className="about__subtitle">4x Distinction Scholar.</span>
             </div>
 
             <div className="about__box">
